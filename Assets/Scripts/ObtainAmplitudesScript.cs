0a784034bb4f535910f8aa6974fdb0f32d39f112
Alan Wills <Alan Wills>
2017-01-20 22:30:38 +0000

    Got beat visualiser working

---
+++ Assets/Scripts/ObtainAmplitudesScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ObtainAmplitudesScript : MonoBehaviour
+{
+    private float[] AudioData;
+    int renderSampleRate = 10;
+
+    public AudioClip Clip { get; private set; }
+
+	// Use this for initialization
+	void Awake ()
+    {
+        Clip = gameObject.GetComponentInChildren<AudioSource>().clip;
+
+        AudioData = new float[Clip.samples * Clip.channels];
+        Clip.GetData(AudioData, 0);
+    }
+
+    public float[] GetRenderData()
+    {
+        int samples_per_100ms = Clip.frequency / renderSampleRate;
+        float[] data = new float[(int)(Clip.length * renderSampleRate * Clip.channels)];
+
+        for (int render_sample = 0; render_sample < data.Length; ++render_sample)
+        {
+            float max = 0;
+            for (int n = 0; n < samples_per_100ms; ++n)
+            {
+                max = Math.Max(max, AudioData[render_sample * samples_per_100ms + n]);
+            }
+
+            data[render_sample] = max;
+        }
+
+        return data;
+    }
+}
---
+++ Assets/Scripts/PriceChangingScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PriceChangingScript : MonoBehaviour {
+
+    private LineRenderer priceLine;
+    private ObtainAmplitudesScript amplitudes;
+
+    private Vector3[] positionBuffer;
+    private float[] amplitudeBuffer;
+    private const float xOffset = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        priceLine = GetComponentInParent<LineRenderer>();
+        amplitudes = GameObject.Find("AudioManager").GetComponent<ObtainAmplitudesScript>();
+        GetComponentInParent<Rigidbody2D>().velocity = new Vector2(-1, 0);
+
+        amplitudeBuffer = amplitudes.GetRenderData();
+        positionBuffer = new Vector3[amplitudeBuffer.Length];
+        priceLine.numPositions = amplitudeBuffer.Length;
+        
+        for (int i = 0; i < amplitudeBuffer.Length; ++i)
+        {
+            positionBuffer[i] = new Vector3(xOffset * i, amplitudeBuffer[i] * 10);
+        }
+
+        priceLine.SetPositions(positionBuffer);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
