53496ba5a5614b6bf3d7aed9bb7d75fb3a447f52
Alan Wills <alawills@googlemail.com>
2017-01-21 21:52:20 +0000

    Added level results screen

---
+++ Assets/Scripts/FinishLevelScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(TransitionToSceneScript))]
+public class FinishLevelScript : MonoBehaviour
+{
+    public static float Player1Money = 0;
+    public static float Player2Money = 0;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponentInParent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (!audioSource.isPlaying)
+        {
+            GetComponentInParent<TransitionToSceneScript>().ForceTransition();
+
+            Player1Money = GameObject.Find("Players/Player1").GetComponent<StockManagementScript>().Money;
+            Player2Money = GameObject.Find("Players/Player2").GetComponent<StockManagementScript>().Money;
+        }
+	}
+}
---
+++ Assets/Scripts/LevelResultsUIScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelResultsUIScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        float player1Score = FinishLevelScript.Player1Money;
+        float player2Score = FinishLevelScript.Player2Money;
+
+        GameObject.Find("Player1UI/MoneyText").GetComponent<Text>().text = player1Score.ToString();
+        GameObject.Find("Player2UI/MoneyText").GetComponent<Text>().text = player2Score.ToString();
+
+        GameObject resultUI = GameObject.Find("ResultUI");
+
+        if (player1Score == player2Score)
+        {
+            resultUI.GetComponentInChildren<Text>().text = "DRAW!!";
+            GameObject.Find("Cup").SetActive(false);
+        }
+        else
+        {
+            if (player2Score > player1Score)
+            {
+                resultUI.transform.Rotate(new Vector3(0, 0, 1), 180);
+            }
+
+            resultUI.transform.Translate(new Vector3(0, -100, 0));
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
