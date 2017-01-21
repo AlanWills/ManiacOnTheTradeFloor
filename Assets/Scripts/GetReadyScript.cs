5b49971d9930cb107e157cfcbcc975d7977c2a2e
Alan Wills <alawills@googlemail.com>
2017-01-21 18:38:00 +0000

    Adding in better 'get ready' textures

---
+++ Assets/Scripts/GetReadyScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetReadyScript : MonoBehaviour
+{
+    public List<Sprite> textures;
+    public float delay = 1.0f;
+
+    private SpriteRenderer spriteRenderer;
+    private float currentTimer = 0;
+    private int currentIndex = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponentInParent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimer += Time.deltaTime;
+
+        if (currentTimer > delay)
+        {
+            if (currentIndex < textures.Count)
+            {
+                spriteRenderer.sprite = textures[currentIndex];
+                currentIndex++;
+                currentTimer = 0;
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        
+    }
+}
