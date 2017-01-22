7d8ea552f9d07a0a00983219d3d7136f412eb9b4
Alan Wills <alawills@googlemail.com>
2017-01-22 13:32:15 +0000

    Added loading and difficulty choosing

---
+++ Assets/Scripts/LoadingScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScript : MonoBehaviour
+{
+    public static string SceneToLoad;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SceneManager.LoadSceneAsync(SceneToLoad);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
